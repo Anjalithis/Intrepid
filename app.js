ed5c6463d41f2a50a872e978f14f15af46bb1299
Anjali Singh <singhanjaliat@gmail.com>
2021-06-05 17:49:00 +0530

    displaying and styling

--- app.js
+++ app.js
@@ -73,7 +73,8 @@ app.post('/campgrounds',validateCampground, catchAsync(async (req,res,next)=>{
   }))
 
 app.get('/campgrounds/:id' , catchAsync(async (req,res)=>{
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground);
     res.render('campgrounds/show',{ campground })
 }));
 
@@ -95,7 +96,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
     res.redirect('/campgrounds');
     }));
 
-app.post('/campgrounds/:id/reviews' , validateReview, catchAsync(async (req,res)=>{
+app.post('/campgrounds/:id/reviews' , validateReview,  catchAsync(async (req,res)=>{
         const campground = await Campground.findById(req.params.id);
         const review = new Review(req.body.review);
         campground.reviews.push(review);
